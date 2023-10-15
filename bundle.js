f17b997d32d72a2c745045d857a58d2bf1754c52
Jean-Baptiste Noblot <noblot.jb@gmail.com>
2023-10-15 22:43:40 +0200

    rollup + bundle

--- bundle.js
+++ bundle.js
@@ -1,5 +1,39 @@
-import { stringify as uuidStringify } from 'uuid';
+var REGEX = /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i;
 
+function validate(uuid) {
+  return typeof uuid === 'string' && REGEX.test(uuid);
+}
+
+/**
+ * Convert array of 16 byte values to UUID string format of the form:
+ * XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+ */
+
+const byteToHex = [];
+
+for (let i = 0; i < 256; ++i) {
+  byteToHex.push((i + 0x100).toString(16).slice(1));
+}
+
+function unsafeStringify(arr, offset = 0) {
+  // Note: Be careful editing this code!  It's been tuned for performance
+  // and works in ways you may not expect. See https://github.com/uuidjs/uuid/pull/434
+  return byteToHex[arr[offset + 0]] + byteToHex[arr[offset + 1]] + byteToHex[arr[offset + 2]] + byteToHex[arr[offset + 3]] + '-' + byteToHex[arr[offset + 4]] + byteToHex[arr[offset + 5]] + '-' + byteToHex[arr[offset + 6]] + byteToHex[arr[offset + 7]] + '-' + byteToHex[arr[offset + 8]] + byteToHex[arr[offset + 9]] + '-' + byteToHex[arr[offset + 10]] + byteToHex[arr[offset + 11]] + byteToHex[arr[offset + 12]] + byteToHex[arr[offset + 13]] + byteToHex[arr[offset + 14]] + byteToHex[arr[offset + 15]];
+}
+
+function stringify(arr, offset = 0) {
+  const uuid = unsafeStringify(arr, offset); // Consistency check for valid UUID.  If this throws, it's likely due to one
+  // of the following:
+  // - One or more input array values don't map to a hex octet (leading to
+  // "undefined" in the uuid)
+  // - Invalid input values for the RFC `version` or `variant` fields
+
+  if (!validate(uuid)) {
+    throw TypeError('Stringified UUID is invalid');
+  }
+
+  return uuid;
+}
 
 const INDEXDB_NAME = 'storyDb';
 
@@ -19,8 +53,7 @@ function DBOnError(event) {
 function DBOnSuccess(event) {
     // Store the result of opening the database in the db variable. This is used a lot below
     db = DBOpenRequest.result;
-};
-
+}
 // This event handles the event whereby a new version of the database needs to be created
 // Either one has not been created before, or a new version number has been submitted via the
 // window.indexedDB.open line above
@@ -43,23 +76,21 @@ function DBOnUpgradeNeeded(event) {
 
 /** @type {FileSystemDirectoryHandle} */
 let dirHandle;
-let packIndexFSDirectoryHandle;
 
 async function getDir() {
     dirHandle = await window.showDirectoryPicker({id: 'studio-js', mode: 'readwrite', startIn: "desktop"});
     /** @type {FileSystemDirectoryHandle} */
     const historyFolderList = await dirHandle.getDirectoryHandle('.content');
-    const packIndexFSDirectoryHandle = await dirHandle.getFileHandle('.pi');
-    const metadataFSDirectoryHandle = await dirHandle.getDirectoryHandle('.md');
+    await dirHandle.getFileHandle('.pi');
+    await dirHandle.getDirectoryHandle('.md');
     for await (const [key, historyFSDirectoryHandle] of historyFolderList.entries()) {
-        historyFSDirectoryHandle.getDirectoryHandle('sf')
+        historyFSDirectoryHandle.getDirectoryHandle('sf');
         console.log({ key, value });
-        readPackIndex()
+        readPackIndex();
     }
 }
 
 async function readPackIndex() {
-    const packUUIDs = [];
     const packIndexFSFileHandle = await dirHandle.getFileHandle('.pi');
     const packIndexFile = await packIndexFSFileHandle.getFile();
     const packIndexStream = packIndexFile.stream();
@@ -67,52 +98,20 @@ async function readPackIndex() {
     let buffer = new Uint8Array();
     for await (const chunk of packIndexStream) {
         const localbuff = new Uint8Array(buffer.length + chunk.length);
-        localbuff.set(buffer)
+        localbuff.set(buffer);
         localbuff.set(chunk, buffer.length);
         const nbLoop = Math.floor(localbuff.length / 16);
         for(let i = 0; i <= nbLoop; i++) {
-            const dv = new DataView(subarray, i * 16, 16)
+            const dv = new DataView(subarray, i * 16, 16);
             //const high = dv.getBigUint64()
             //const low = dv.getBigUint64(8)
 
-            uuidStringify(dv.buffer)
+            stringify(dv.buffer);
         }
         buffer = localbuff.subarray(i * 16);
       }
 
 
-}
-
-function getNodeIndex() {
-    return "ni";
-}
-
-function getListIndex() {
-    return ("li");
-}
-
-function getImageIndex() {
-    return ("ri");
-}
-
-function getImageFolder() {
-    return ("rf");
-}
-
-function getSoundIndex() {
-    return ("si");
-}
-
-function getSoundFolder() {
-    return ("sf");
-}
-
-function getNightMode() {
-    return ("nm");
-}
-
-function getBoot() {
-    return ("bt");
 }
 
 
